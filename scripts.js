368b296678515cf51a6b5c250fd5266daff99bf7
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-07-30 20:14:44 -0300

    DBI Final

---
+++ scripts.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var headerHeight = document.querySelector("header").offsetHeight;
+  var navbar = document.getElementById("myNavbar");
+
+  function toggleNavbarClass() {
+    if (window.pageYOffset > headerHeight) {
+      navbar.classList.add("navbar-scroll");
+      navbar.classList.remove("navbar-scrollB");
+    } else {
+      navbar.classList.remove("navbar-scroll");
+      navbar.classList.add("navbar-scrollB");
+    }
+  }
+
+  toggleNavbarClass();
+  window.addEventListener("scroll", toggleNavbarClass);
+});
+
+window.sr = ScrollReveal();
+sr.reveal(`.about__content`, {
+  duration: 3000,
+  origin: `top`,
+  distance: `-50px`,
+});
+sr.reveal(`.about__picture`, {
+  duration: 3000,
+  origin: `bottom`,
+  distance: `-50px`,
+});
+sr.reveal(`.contact-container`, {
+  duration: 5000,
+});
+
+sr.reveal(`.form-container`, {
+  duration: 3000,
+  origin: `right`,
+  distance: `-500px`,
+});
+sr.reveal(`.form-container h3`, {
+  duration: 3000,
+  origin: `bottom`,
+  distance: `-100px`,
+});
+
+sr.reveal(`.map`, {
+  duration: 3000,
+  origin: `left`,
+  distance: `-500px`,
+});
+
+sr.reveal("#leng-button", {
+  duration: 3000,
+  origin: `left`,
+  distance: `-500px`,
+});
+
+sr.reveal("#construction-title", {
+  duration: 3000,
+  origin: `bottom`,
+  distance: `-100px`,
+});
+
+sr.reveal("#carousel-img", {
+  duration: 3000,
+  origin: `right`,
+  distance: `-100px`,
+});
+
+sr.reveal("#work-title", {
+  duration: 3000,
+  origin: `right`,
+  distance: `-100px`,
+});
+
+sr.reveal("#work-subtitle", {
+  duration: 5000,
+  origin: `left`,
+  distance: `-100px`,
+});
+
+sr.reveal("#work-button", {
+  duration: 6000,
+  origin: `left`,
+  distance: `-100px`,
+});
